9535cf2e7d057659fa087e709a62cd1edd68cf9b
Hector Salas Serrano <kenkira666@gmail.com>
2023-06-16 13:52:21 -0500

    added changes

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Hector";
+            int myAge = 37;
+            char myChar ='H';
+            bool isSmart = true;
+            double myHeight = 6.1;
+            decimal myJob = 15.5m;
+
+            Console.WriteLine($"My name is {myName}, and I am {myAge} years old.");
+            Console.WriteLine($"My name starts with {myChar} , andd that is {isSmart} to point out.");
+            Console.WriteLine($"My height is {myHeight} ft. and my job hourly pay is {myJob} dollars per hour.");
+
+ 
+
+
         }
     }
 }
